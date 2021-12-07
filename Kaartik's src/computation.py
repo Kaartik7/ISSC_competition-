0894595daa8b1b4280131b5b89aad468d91f5b8b
Kaartik7 <50298143+Kaartik7@users.noreply.github.com>
2021-12-07 11:10:07 -0500

    Create computation.py

---
+++ Kaartik's src/computation.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import csv
+
+lst_tuberclosis = []
+lst_giardiasis = []
+lst_influenza = []
+lst_para = []
+lst_malaria = []
+
+with open('ratesbydisease2008_2018.csv', encoding="utf8", errors='ignore') as CSV_FILE2:
+    HEALTH_DATA = csv.reader(CSV_FILE2)
+    next(HEALTH_DATA)  # to skip the first row
+    for row in HEALTH_DATA:
+        if row[0] == 'Tuberculosis':
+            for i in range(1, len(row) ):
+                if i % 2 != 0:
+                    lst_tuberclosis.append(row[i])
+
+with open('ratesbydisease2008_2018.csv', encoding="utf8", errors='ignore') as CSV_FILE2:
+    HEALTH_DATA = csv.reader(CSV_FILE2)
+    next(HEALTH_DATA)  # to skip the first row
+    for row in HEALTH_DATA:
+        if row[0] == 'Influenza ':
+           for i in range(1, len(row)):
+                if i % 2 != 0:
+                    lst_influenza.append(row[i])
+
+with open('ratesbydisease2008_2018.csv', encoding="utf8", errors='ignore') as CSV_FILE2:
+    HEALTH_DATA = csv.reader(CSV_FILE2)
+    next(HEALTH_DATA)  # to skip the first row
+    for row in HEALTH_DATA:
+        if row[0] == 'Malaria':
+            for i in range(1, len(row)):
+                if i % 2 != 0:
+                    lst_malaria.append(row[i])
+
+with open('ratesbydisease2008_2018.csv', encoding="utf8", errors='ignore') as CSV_FILE2:
+    HEALTH_DATA = csv.reader(CSV_FILE2)
+    next(HEALTH_DATA)  # to skip the first row
+    for row in HEALTH_DATA:
+        if row[0] == 'Giardiasis':
+            for i in range(1, len(row)):
+                if i % 2 != 0:
+                    lst_giardiasis.append(row[i])
+
+with open('ratesbydisease2008_2018.csv', encoding="utf8", errors='ignore') as CSV_FILE2:
+    HEALTH_DATA = csv.reader(CSV_FILE2)
+    next(HEALTH_DATA)  # to skip the first row
+    for row in HEALTH_DATA:
+        if row[0] == 'Paratyphoid fever':
+            for i in range(1, len(row)):
+                if i % 2 != 0:
+                    lst_para.append(row[i])
+
+
